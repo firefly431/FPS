a6543f823d6f4676f56a8a9328cbc2de942091f7
firefly431 <firefly431.awesome@gmail.com>
2015-03-26 13:34:07 -0500

    Retab

--- vec4.h
+++ vec4.h
@@ -2,9 +2,9 @@
 #define _INCLUDED_VEC3_H
 
 struct vec4 {
-	double x, y, z, w;
+    double x, y, z, w;
 
-	vec4(double x, double y, double z, double w) : x(x), y(y), z(z), w(w) {}
+    vec4(double x, double y, double z, double w) : x(x), y(y), z(z), w(w) {}
 };
 
 #endif
--- vector.h
+++ vector.h
@@ -4,107 +4,107 @@
 #include <cmath>
 
 struct vector {
-	double x, y;
-	inline vector() : x(0), y(0) {}
-	inline vector(const double x, const double y) : x(x), y(y) {}
-	inline vector(const vector &other): x(other.x), y(other.y) {}
+    double x, y;
+    inline vector() : x(0), y(0) {}
+    inline vector(const double x, const double y) : x(x), y(y) {}
+    inline vector(const vector &other): x(other.x), y(other.y) {}
 
-	inline const vector &operator=(const vector &other) {
-		x = other.x;
-		y = other.y;
-		return *this;
-	}
+    inline const vector &operator=(const vector &other) {
+        x = other.x;
+        y = other.y;
+        return *this;
+    }
 
-	inline vector operator+(const vector &other) const {
-		return vector(other.x + x, other.y + y);
-	}
+    inline vector operator+(const vector &other) const {
+        return vector(other.x + x, other.y + y);
+    }
 
-	inline vector operator-(const vector &other) const {
-		return vector(other.x - x, other.y - y);
-	}
+    inline vector operator-(const vector &other) const {
+        return vector(other.x - x, other.y - y);
+    }
 
-	// actually dot product
-	inline double operator*(const vector &other) const {
-		return other.x * x + other.y * y;
-	}
+    // actually dot product
+    inline double operator*(const vector &other) const {
+        return other.x * x + other.y * y;
+    }
 
-	inline vector operator/(const vector &other) const {
-		return vector(other.x / x, other.y / y);
-	}
+    inline vector operator/(const vector &other) const {
+        return vector(other.x / x, other.y / y);
+    }
 
-	inline vector operator*(const double fac) const {
-		return vector(x * fac, y * fac);
-	}
+    inline vector operator*(const double fac) const {
+        return vector(x * fac, y * fac);
+    }
 
-	inline vector operator/(const double fac) const {
-		return vector(x / fac, y / fac);
-	}
+    inline vector operator/(const double fac) const {
+        return vector(x / fac, y / fac);
+    }
 
-	inline vector &operator+=(const vector &other) {
-		x += other.x;
-		y += other.y;
-		return *this;
-	}
+    inline vector &operator+=(const vector &other) {
+        x += other.x;
+        y += other.y;
+        return *this;
+    }
 
-	inline vector &operator-=(const vector &other) {
-		x -= other.x;
-		y -= other.y;
-		return *this;
-	}
+    inline vector &operator-=(const vector &other) {
+        x -= other.x;
+        y -= other.y;
+        return *this;
+    }
 
-	inline vector &operator*=(const vector &other) {
-		x *= other.x;
-		y *= other.y;
-		return *this;
-	}
+    inline vector &operator*=(const vector &other) {
+        x *= other.x;
+        y *= other.y;
+        return *this;
+    }
 
-	inline vector &operator/=(const vector &other) {
-		x /= other.x;
-		y /= other.y;
-		return *this;
-	}
+    inline vector &operator/=(const vector &other) {
+        x /= other.x;
+        y /= other.y;
+        return *this;
+    }
 
-	inline vector &operator*=(const double other) {
-		x *= other;
-		y *= other;
-		return *this;
-	}
+    inline vector &operator*=(const double other) {
+        x *= other;
+        y *= other;
+        return *this;
+    }
 
-	inline vector &operator/=(const double other) {
-		x *= other;
-		y *= other;
-		return *this;
-	}
+    inline vector &operator/=(const double other) {
+        x *= other;
+        y *= other;
+        return *this;
+    }
 
-	inline double operator+(void) const {
-		return std::sqrt(x * x + y * y);
-	}
+    inline double operator+(void) const {
+        return std::sqrt(x * x + y * y);
+    }
 
-	inline double sqr(void) const {
-		return x * x + y * y;
-	}
+    inline double sqr(void) const {
+        return x * x + y * y;
+    }
 
-	inline void normalize(void) {
-		(*this) /= +(*this);
-	}
+    inline void normalize(void) {
+        (*this) /= +(*this);
+    }
 
-	inline vector normalized(void) const {
-		return (*this) / +(*this);
-	}
+    inline vector normalized(void) const {
+        return (*this) / +(*this);
+    }
 
-	inline double angle(void) const {
-		return std::atan2(y, x);
-	}
+    inline double angle(void) const {
+        return std::atan2(y, x);
+    }
 
-	inline double dist(const vector &other) const {
-		return +(*this - other);
-	}
+    inline double dist(const vector &other) const {
+        return +(*this - other);
+    }
 
-	inline double dot(const vector &other) const {
-		return x * other.x + y * other.y;
-	}
+    inline double dot(const vector &other) const {
+        return x * other.x + y * other.y;
+    }
 
-	// TODO: project point onto line
+    // TODO: project point onto line
 };
 
 #endif

9bf480ceacf41aee63d1943dadf2ea7b5b1a0da4
firefly431 <firefly431.awesome@gmail.com>
2015-04-07 21:24:53 -0500

    Typo

--- Buffer.cpp
+++ Buffer.cpp
@@ -1,7 +1,8 @@
 #include "Buffer.h"
 
-#ifdef _MSC_VER
+#ifndef _MSC_VER
 template class Buffer<GL_UNIFORM_BUFFER>;
 template class Buffer<GL_ARRAY_BUFFER>;
 template class Buffer<GL_ELEMENT_ARRAY_BUFFER>;
 #endif
+

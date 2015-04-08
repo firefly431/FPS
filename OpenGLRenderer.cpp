54b9a7a25807a7c4fc2c1b5578b03b8444c6440e
firefly431 <firefly431.awesome@gmail.com>
2015-04-07 21:21:06 -0500

    Fix indentation

--- OpenGLRenderer.cpp
+++ OpenGLRenderer.cpp
@@ -11,18 +11,15 @@
 #if defined(SFML_SYSTEM_WINDOWS)
 #include "wglext.h"
 
-void *loadGLFunc(const char *name)
-{
-  void *p = (void *)wglGetProcAddress(name);
-  if(p == 0 ||
-    (p == (void*)0x1) || (p == (void*)0x2) || (p == (void*)0x3) ||
-    (p == (void*)-1) )
-  {
-    HMODULE module = LoadLibraryA("opengl32.dll");
-    p = (void *)GetProcAddress(module, name);
-  }
- 
-  return p;
+void *loadGLFunc(const char *name) {
+    void *p = (void *)wglGetProcAddress(name);
+    if(p == 0 ||
+       (p == (void *)0x1) || (p == (void *)0x2) || (p == (void *)0x3) ||
+       (p == (void *)-1)) {
+        HMODULE module = LoadLibraryA("opengl32.dll");
+        p = (void *)GetProcAddress(module, name);
+    }
+    return p;
 }
 
 #endif
